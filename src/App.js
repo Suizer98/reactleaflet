1fd513e70114467489149dcd5183526e1428010d
Suizer <teysuizer1998@gmail.com>
2022-08-15 08:58:48 +0800

    gg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import './App.css';
 
-function App() {
+const center = [1.3411547188393913, 103.81687980973597];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<MapContainer center={center} zoom={13}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={center}>
+      <Popup>
+        {center}
+      </Popup>
+    </Marker>
+  </MapContainer>
   );
 }
 
-export default App;
+
