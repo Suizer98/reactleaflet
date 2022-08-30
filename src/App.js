f6cd9d5e5c1e0f348e3714ddfcffd2ba838d1a82
Suizer <teysuizer1998@gmail.com>
2022-08-30 15:10:12 +0800

    jsonfull

--- src/App.js
+++ src/App.js
@@ -9,23 +9,20 @@ const center = [1.3411547188393913, 103.81687980973597];
 
 export default function App() {
   return (
-<MapContainer center={center} zoom={13}>
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
+    <MapContainer center={center} zoom={13}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
 
+      {teslajson.map(tsla => (
+        <Marker         
+        key = {tsla.id}
+        position={[tsla.gps.latitude, tsla.gps.longitude]}>
+        </Marker>
+      ))}
 
-    <Marker position={center}>
-
-      <Popup>
-        A popup <br/> Easily customisable
-      </Popup>
-
-    </Marker>
-
-    
-  </MapContainer>
+    </MapContainer>
   );
 }
 
